c38d990879f9c663ac9e3bcbdf824413638c900b
Abhay Kumar Nishad <abhayn927@gmail.com>
2026-07-18 19:22:51 +0530

    Update Solution.java

--- src/Standard Stack/946 Validate Stack Sequence/Solution.java
+++ src/Standard Stack/946 Validate Stack Sequence/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     // TC-> O(n) SC-> O(n)
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> st=new Stack<>();
-        int idx=0;
+        Stack<Integer> st = new Stack<>();
+        int idx = 0;
         for(int x : pushed) {
             st.push(x);
             while(!st.isEmpty() && st.peek() == popped[idx]) {
